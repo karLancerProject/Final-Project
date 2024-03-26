fb681fdf88029c16423d263e75996ae706922eaf
omid <omid.jalali.773@gmail.com>
2024-03-26 16:08:03 +0330

    updated

--- pages/index.js
+++ pages/index.js
@@ -39,17 +39,13 @@ export default function Home() {
   });
 
   async function connect() {
-    if (typeof window.ethereum !== "undefined") {
-      try {
-        const web3ModalProvider = await web3Modal.connect();
-        setIsConnected(true);
-        const provider = new ethers.providers.Web3Provider(web3ModalProvider);
-        setSigner(provider.getSigner());
-      } catch (e) {
-        console.log(e);
-      }
-    } else {
-      setIsConnected(false);
+    try {
+      const web3ModalProvider = await web3Modal.connect();
+      setIsConnected(true);
+      const provider = new ethers.providers.Web3Provider(web3ModalProvider);
+      setSigner(provider.getSigner());
+    } catch (e) {
+      console.log(e);
     }
   }
 

d8ae66d0d99fe8272378971f0443d0152d27c1c7
omid <omid.jalali.773@gmail.com>
2024-03-26 15:56:33 +0330

    created

---
+++ pages/index.js
@@ -0,0 +1,73 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+const inter = Inter({ subsets: ["latin"] });
+
+import Web3Modal from "web3modal";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+
+let web3Modal;
+
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider, // required
+    options: {
+      rpc: { 42: process.env.NEXT_PUBLIC_RPC_URL }, // required
+    },
+  },
+};
+
+if (typeof window !== "undefined") {
+  web3Modal = new Web3Modal({
+    cacheProvider: false,
+    providerOptions, // required
+  });
+}
+
+export default function Home() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [hasMetamask, setHasMetamask] = useState(false);
+  const [signer, setSigner] = useState(undefined);
+
+  useEffect(() => {
+    if (typeof window.ethereum !== "undefined") {
+      setHasMetamask(true);
+    }
+  });
+
+  async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+      try {
+        const web3ModalProvider = await web3Modal.connect();
+        setIsConnected(true);
+        const provider = new ethers.providers.Web3Provider(web3ModalProvider);
+        setSigner(provider.getSigner());
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      setIsConnected(false);
+    }
+  }
+
+  
+
+  return (
+    <div>
+      {" "}
+      {hasMetamask ? (
+        isConnected ? (
+          "Connected! "
+        ) : (
+          <button onClick={() => connect()}>Connect</button>
+        )
+      ) : (
+        "Please install metamask"
+      )}
+      {isConnected ? <button onClick={() => execute()}>Execute</button> : ""}
+    </div>
+  );
+}
